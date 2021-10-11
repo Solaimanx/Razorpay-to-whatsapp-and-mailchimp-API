1055982643dfd7c8ec6839f072e5a0efd12312b2
Mohammad Solaiman <solaiman321@gmail.com>
2021-10-11 16:57:25 +0600

    add mailchimp endpoint to add/remove tags

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
-const Razorpay = require("razorpay")
-var Mailchimp = require('mailchimp-api-v3')
+const Razorpay = require("razorpay");
+var Mailchimp = require("mailchimp-api-v3");
+var md5 = require("md5");
 const app = express();
 const cors = require("cors");
 app.use(express.json());
 const corsOptions = {
   origin: "*",
-  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS","PATCH"],
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
   allowedHeaders: [
@@ -23,25 +24,23 @@ const corsOptions = {
     "Accept",
   ],
 };
-app.use(cors(corsOptions)); 
-
+app.use(cors(corsOptions));
 
 const instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
+var mailchimp = new Mailchimp(process.env.MAILCHIMP_KEY);
 
 app.get("/", (req, res) => {
   res.send({
-    server : "running"
-  })
-})
-
-
+    server: "running",
+  });
+});
 
 ///craete order id
-app.get("/create-order", (req, res,next) => {
+app.get("/create-order", (req, res, next) => {
   const options = {
     amount: 299, // amount in the smallest currency unit
     currency: "INR",
@@ -49,37 +48,107 @@ app.get("/create-order", (req, res,next) => {
   };
   instance.orders.create(options, function (err, order) {
     console.log(order);
-    res.send({orderId : order.id})
+    res.send({ orderId: order.id });
+  });
+});
+
+///get customer info
+app.post("/userinfo", (req, res, next) => {
+  const { payment_id } = req.body;
+
+  instance.payments.fetch(payment_id).then((response) => {
+    console.log(response.email);
+    res.send({ email: response.email, number: response.contact });
   });
 });
 
 
-///get customer info 
-app.post("/userinfo", (req, res,next) =>{
-  const { payment_id } = req.body
+////mailchimp api start from here
 
-    instance.payments.fetch(payment_id)
-    .then((response)=>{
-        console.log(response.email);
-        res.send({email : response.email , number : response.contact})
+//// sucess payment
+app.get("/mc/add-success-tag/:email", (req, res, next) => {
+  const { email } = req.params;
+
+  const hashedEmail = md5(email);
+
+  mailchimp
+    .post({
+      path: `/lists/f22669270c/members/${hashedEmail}/tags`,
+      body: {
+        tags: [
+          {
+            name: "HackerrankPaid",
+            status: "active",
+          },
+        ],
+      },
     })
+    .then((response) => {
+      if (response.statusCode == 204) {
+        mailchimp
+          .post({
+            path: `/lists/f22669270c/members/${hashedEmail}/tags`,
+            body: {
+              tags: [
+                {
+                  name: "HackerrankFollowup",
+                  status: "inactive",
+                },
+              ],
+            },
+          })
+          .then((response) => {
+            if (response.statusCode == 204) {
+              next()
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        return res.status(422).json({ message: "failed to add tag" });
+      }
+      
+      return res.status(200).json({ message: "sucess" });
 
-})
+
+  
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 
+///failed payment 
 
-////mailchimp api
+app.get("/mc/add-followup-tag/:email", (req, res, next) => {
+  const { email } = req.params;
 
+  const hashedEmail = md5(email);
 
+  mailchimp
+    .post({
+      path: `/lists/f22669270c/members/${hashedEmail}/tags`,
+      body: {
+        tags: [
+          {
+            name: "HackerrankFollowup",
+            status: "active",
+          },
+        ],
+      },
+    })
+    .then((response) => {
 
-
-
-
-
-
-
-
-
+      if(response.statusCode == 204) {
+        return res.status(200).json({ message: "sucess" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 
 
